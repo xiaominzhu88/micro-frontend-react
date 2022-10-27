7997b691a42d0ee5eaf3ebc82d842024f57b30f2
xiaomin zhu <xiaomin.zhu88@gmail.com>
2022-10-27 19:59:08 +0200

    add mui theme, fix module export

--- libs/common-ui/src/tsremote_declarations.ts
+++ libs/common-ui/src/tsremote_declarations.ts
@@ -1,4 +1,2 @@
 declare module '@micro-frontend-react/fetch';
-declare module './lib/components/characters';
-declare module './lib/components/microReactComponent';
 declare module '@micro-frontend-react/common-ui';
